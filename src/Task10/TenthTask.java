9e44a0d5851f902ce22141f0c7dc9a16a456ca6b
marynazhulai <marynazhulai@gmail.com>
2021-03-21 23:09:39 +0200

    task10

--- src/Task10/TenthTask.java
+++ src/Task10/TenthTask.java
@@ -1,7 +1,27 @@
 package Task10;
+import java.util.ArrayList;
 
 public class TenthTask {
     public static void main(String[] args) {
 
+        printList(transformIntToChar ( 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100));
     }
-}
+
+        private static ArrayList<Character> transformIntToChar ( int...values){
+            ArrayList<Character> buffer = new ArrayList<>();
+            for (int i : values) {
+                buffer.add(((char) i));
+            }
+            return buffer;
+        }
+
+        private static void printList (ArrayList < Character > characters) {
+                for (char c : characters) {
+                    System.out.print(c);
+                }
+        }
+    }
+
+
+
+
